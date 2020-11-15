772a1dc24a22c3d973730547e78545687f039e9f
ANIRUDHA KULKARNI <kulkarnianirudha8@gmail.com>
2020-11-16 02:24:58 +0530

    Implemented A1 part
    almost all functions except sanity check

--- stubsA123/A1List.java
+++ stubsA123/A1List.java
@@ -24,6 +24,7 @@ public class A1List extends List {
     {
         A1List new_node=new A1List(address,size,key);
         new_node.next=this.next;
+        this.next.prev=new_node;
         this.next=new_node;
         new_node.prev=this;
         return new_node;
@@ -31,52 +32,116 @@ public class A1List extends List {
 
     public boolean Delete(Dictionary d) 
     {
-        if(this.key==d.key){
-            
+        A1List position=this.Find(d.key, true);
+        if(position==null){
+            return false;            
         }
-        
-        A1List head_node=
 
-        while(this!=null){
-            if(this.key==d.key){
-                
-            }
-        }
-        return false;
+        position.prev.next=position.next;
+        position.next.prev=position.prev;
+        position.next=null;position.prev=null;
+        return true;
     }
 
     public A1List Find(int k, boolean exact)
-    { 
-
+    {
+        if(exact==true){
+            A1List current=this;
+            while(!isTailsent(current)){
+                if(current.key==k){
+                    return current;
+                }
+                current=current.next;
+            }
+            current=this;
+            while(!isHeadsent(current)){
+                if(current.key==k){
+                    return current;
+                }
+                current=current.prev;
+            }
+            return null;
+        }
+        if(exact==false){
+            A1List current=this;
+            while(!isTailsent(current)){
+                if(current.key>=k){
+                    return current;
+                }
+                current=current.next;
+            }
+            current=this;
+            while(!isHeadsent(current)){
+                if(current.key>=k){
+                    return current;
+                }
+                current=current.prev;
+            }
+            return null;
+        }
         return null;
     }
 
     public A1List getFirst()
     {
-        if(this.prev==null){
-            return this;
+        //if node is null or list is empty
+        if((this==null)||(this.prev==null && this.next.next==null)||(this.next==null && this.prev.prev==null)){
+            return null;
         }
-        A1List temp_prev=this.prev;
-        while(temp_prev!=null){
+        //we have DLL with atleast 1 element and 2 sentinal nodes
+        A1List temp_prev=this;
+        if(this.prev==null){
+            temp_prev=this.next;    
+        }
+        //traverse till find headsentinal and then output next element
+        while(!isHeadsent(temp_prev)){
             temp_prev=temp_prev.prev;
         }
-        return temp_prev;
+        return temp_prev.next;
     }
     
     public A1List getNext() 
     {
+        // If the element is the last element of DLL then we should return null instead of tail sentinal node
+        // Assume: No tail sentinal is given as input. if given return null
+        if(this==null || isTailsent(this)){
+            return null;
+        }
+        if(isTailsent(this.next)){
+            return null;
+        }
         return this.next;
     }
 
     public boolean sanity()
     {
+        
         return true;
     }
+    private boolean isHeadsent(A1List testnode){
+        if(testnode==null){
+            return false;
+        }
+        if(testnode.address==-1 && testnode.size==-1 && testnode.key==-1 &&testnode.prev==null){
+            return true;
+        }
+        return false;
+    }
+    private boolean isTailsent(A1List testnode){
+        if(testnode.address==-1 && testnode.size==-1 && testnode.key==-1 &&testnode.next==null){
+            return true;
+        }
+        return false;
+    }
+    //MAKE SURE TO DELETE MAIN FUNCTION BEFORE SUBMITTING
     public static void main(String[] args) {
-        A1List test=new A1List(112233,10,1);
+        A1List test=new A1List();
+        System.out.println("getFirst = "+test.getFirst());
+        System.out.println("getNext = "+test.getNext());
         test.Insert(112234, 20, 2);
-        System.out.println(test.next.key);
-
+        System.out.println("getFirst = "+test.getFirst().key);
+        System.out.println("getNext = "+test.getNext().key);
+        System.out.println(test.key);
     }
 
 }
